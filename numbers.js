11ebc0d2edbf9ed22dfc222404e143ad432fcf2c
Ararat Asatryan <ar.asatryan94@gmail.com>
2026-06-01 13:18:01 +0400

    dev:data-types-and-loops

---
+++ numbers.js
@@ -0,0 +1,22 @@
+
+
+
+// 1.Արտածել 1-ից մինչև N բոլոր ամբողջ թվերը։
+// 2.Արտածել 1-ից մինչև N բոլոր ամբողջ զույգ թվերը։
+
+
+
+const n=50; 
+for(let i= 1;i<=n;i++)
+
+{
+if (i%2=== 0)
+{
+    console.log(i);
+}
+
+}
+
+let name="ovsanna";
+console.log(n.lenght)
+console.log(name.lenght)
