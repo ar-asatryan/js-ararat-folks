04ae6b987da8fa14b5b6bbc0afd88d243386c130
ar-asatryan <ar.asatryan94@gmail.com>
2026-06-03 11:02:23 +0400

    testing-git-contributions

--- numbers.js
+++ numbers.js
@@ -19,4 +19,10 @@ if (i%2=== 0)
 
 let name="ovsanna";
 console.log(n.lenght)
-console.log(name.lenght)
+//console.log(name.lenght)
+
+
+const testArray=[1,2,3,4,5];
+
+const stringArray=["Stark","Strange","Banner","Rogers","Yung","Man"];
+console.log("testArray length: ", stringArray.length)
